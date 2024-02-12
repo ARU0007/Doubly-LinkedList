da0c30e5b9332ecd8b5b832bf5b3a57d6b4d063d
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-12 19:19:34 +0530

    create main method to call creation and traverser. .

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import list.linked.DoublyLL;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLL doublyLL = new DoublyLL();
+        doublyLL.creation();
+        doublyLL.traverser();
+
+
+    }
+
+}
